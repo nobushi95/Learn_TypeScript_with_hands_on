0c28bafc9f8f4fa5abd299ccea6ee00828120783
nobushi95 <nobushi1121@gmail.com>
2022-09-04 22:44:01 +0900

    HitAndBlowクラスの実装

--- ch03/node-app/src/index.ts
+++ ch03/node-app/src/index.ts
@@ -1,3 +1,60 @@
+class HitAndBlow {
+  private readonly answerSource = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+  private answer: string[] = [];
+  private tryCount = 0;
+
+  setting() {
+    const answerLength = 3;
+
+    while (this.answer.length < answerLength) {
+      const randNum = Math.floor(Math.random() * this.answerSource.length);
+      const selectedItem = this.answerSource[randNum];
+      if (!this.answer.includes(selectedItem)) this.answer.push(selectedItem);
+    }
+  }
+
+  async play() {
+    const inputArr = (
+      await promptInput("「,」区切りで3つの数字を入力してください")
+    ).split(",");
+    const result = this.check(inputArr);
+
+    if (result.hit != this.answer.length) {
+      printLine(`---\nHit: ${result.hit}\nBlow: ${result.blow}\n---`);
+      this.tryCount += 1;
+      await this.play();
+    } else {
+      this.tryCount += 1;
+    }
+  }
+
+  private check(input: string[]) {
+    let hitCount = 0;
+    let blowCount = 0;
+
+    input.forEach((val, index) => {
+      if (val === this.answer[index]) hitCount += 1;
+      else if (this.answer.includes(val)) blowCount += 1;
+    });
+
+    return {
+      hit: hitCount,
+      blow: blowCount,
+    };
+  }
+}
+
 const printLine = (text: string, breakLine: boolean = true) => {
   process.stdout.write(text + (breakLine ? "\n" : ""));
 };
@@ -11,9 +68,7 @@ const promptInput = async (text: string) => {
 };
 
 (async () => {
-  const name = await promptInput("名前を入力してください");
-  console.log(name);
-  const age = await promptInput("年齢を入力してください");
-  console.log(age);
-  process.exit();
+  const hitAndBlow = new HitAndBlow();
+  hitAndBlow.setting();
+  await hitAndBlow.play();
 })();
