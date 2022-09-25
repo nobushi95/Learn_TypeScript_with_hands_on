e289152d258c4f4158f44b15a9de45e7d1ef3b1c
nobushi95 <nobushi1121@gmail.com>
2022-09-25 12:22:39 +0900

    ユーティリティ関数をソース上部に移動

--- ch03/node-app/src/index.ts
+++ ch03/node-app/src/index.ts
@@ -1,3 +1,37 @@
+const printLine = (text: string, breakLine: boolean = true) => {
+  process.stdout.write(text + (breakLine ? "\n" : ""));
+};
+
+const readLine = async () => {
+  const input: string = await new Promise((resolve) =>
+    process.stdin.once("data", (data) => resolve(data.toString()))
+  );
+  return input.trim();
+};
+
+const promptInput = async (text: string) => {
+  printLine(`\n${text}\n`, false);
+  return readLine();
+};
+
+const promptSelect = async <T extends string>(
+  text: string,
+  values: readonly T[]
+): Promise<T> => {
+  printLine(`\n${text}`);
+  values.forEach((v) => {
+    printLine(`- ${v}`);
+  });
+  printLine(`> `, false);
+
+  const input = (await readLine()) as T;
+  if (values.includes(input)) {
+    return input;
+  } else {
+    return promptSelect(text, values);
+  }
+};
+
 const modes = ["normal", "hard"] as const;
 type Mode = typeof modes[number];
 
@@ -96,40 +130,6 @@ class HitAndBlow {
     return isLengthValid && isAllAnserSourceOption && isAllDifferentValues;
   }
 }
-
-const printLine = (text: string, breakLine: boolean = true) => {
-  process.stdout.write(text + (breakLine ? "\n" : ""));
-};
-
-const readLine = async () => {
-  const input: string = await new Promise((resolve) =>
-    process.stdin.once("data", (data) => resolve(data.toString()))
-  );
-  return input.trim();
-};
-
-const promptInput = async (text: string) => {
-  printLine(`\n${text}\n`, false);
-  return readLine();
-};
-
-const promptSelect = async <T extends string>(
-  text: string,
-  values: readonly T[]
-): Promise<T> => {
-  printLine(`\n${text}`);
-  values.forEach((v) => {
-    printLine(`- ${v}`);
-  });
-  printLine(`> `, false);
-
-  const input = (await readLine()) as T;
-  if (values.includes(input)) {
-    return input;
-  } else {
-    return promptSelect(text, values);
-  }
-};
 
 (async () => {
   const hitAndBlow = new HitAndBlow();
