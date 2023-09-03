4b107dfd14173387d3d359da2addc1fba1c70b2c
nobushi95 <nobushi1121@gmail.com>
2023-09-03 15:27:44 +0900

    Taskの状態をenumで定義

--- ch04/browser-app/ts/Task.ts
+++ ch04/browser-app/ts/Task.ts
@@ -1,11 +1,19 @@
 import { v4 as uuid } from 'uuid';
 
+enum Status {
+    Todo = 'TODO',
+    Doing = 'DOING',
+    Done = 'DONE',
+};
+
 export class Task {
-    readonly id;
-    title;
+    readonly id: string;
+    title: string;
+    status: Status;
 
     constructor(properties: { tittle: string }) {
         this.id = uuid();
         this.title = properties.tittle;
+        this.status = Status.Todo;
     }
 }
