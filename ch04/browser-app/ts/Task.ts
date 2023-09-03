e908183f0a879bb1da770eecacd07e20dd67e219
nobushi95 <nobushi1121@gmail.com>
2023-09-03 15:12:19 +0900

    Taskにidを付与できるように変更

--- ch04/browser-app/ts/Task.ts
+++ ch04/browser-app/ts/Task.ts
@@ -1,7 +1,11 @@
-export class Task{
-    title: string;
+import { v4 as uuid } from 'uuid';
+
+export class Task {
+    readonly id;
+    title;
 
     constructor(properties: { tittle: string }) {
+        this.id = uuid();
         this.title = properties.tittle;
     }
 }
