eb0d3477fb29108c51bc7776535d2c970c96decd
nobushi95 <nobushi1121@gmail.com>
2023-10-01 22:18:57 +0900

    Headingコンポーネント追加

--- ch05/react-app/src/App.tsx
+++ ch05/react-app/src/App.tsx
@@ -1,5 +1,14 @@
+import { Heading } from "./libs/Heading";
 import { Text } from "./libs/Text";
 
 export const App = () => {
-  return <Text text={"true"} />;
+  return (
+    <>
+      <Text text={"true"} />
+      <Heading tag="h1">見出し</Heading>
+      <Heading tag="h1">
+        <span>hello, world!</span>
+      </Heading>
+    </>
+  );
 };
