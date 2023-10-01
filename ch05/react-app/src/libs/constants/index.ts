9a3ef18bc3f5880db7428b8d7a6615174ce4abd0
nobushi95 <nobushi1121@gmail.com>
2023-10-01 17:34:03 +0900

    定数ファイル追加

---
+++ ch05/react-app/src/libs/constants/index.ts
@@ -0,0 +1,17 @@
+export const radius = {
+  s: "4px",
+  m: "8px",
+  l: "16px",
+} as const;
+
+export const space = {
+  s: "4px",
+  m: "8px",
+  l: "16px",
+} as const;
+
+export const fontSize = {
+  s: "10px",
+  m: "16px",
+  l: "24px",
+} as const;
